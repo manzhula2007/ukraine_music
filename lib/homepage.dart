c3b4542a5039d7b9ae6ec40752967b9f40cc6a2a
Yehor Manzhula <m.egor2007@gmail.com>
2022-10-20 20:37:15 +0300

    Grid added

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -12,10 +12,16 @@ class Homepage extends StatelessWidget {
         title: Text("Українська музика з Незалежності"),
         backgroundColor: Colors.orangeAccent,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextButton(
+
+      body: GridView.count(
+        scrollDirection: Axis.vertical,
+        primary: false,
+        padding: const EdgeInsets.all(20),
+        crossAxisSpacing: 10,
+        mainAxisSpacing: 10,
+        crossAxisCount: 2,
+        children: <Widget>[
+        ElevatedButton(
               onPressed: () {
                 Navigator.of(context).push(
                   MaterialPageRoute(
@@ -23,20 +29,82 @@ class Homepage extends StatelessWidget {
                   ),
                 );
               },
+            style: ElevatedButton.styleFrom(
+              // side: BorderSide(width:3, color:Colors.brown),
+              backgroundColor: Colors.blue, // background (button) color
+              foregroundColor: Colors.black, // foreground (text) color
+            ),
               child: Text("Що було до незалежності")
           ),
-          TextButton(
-              onPressed: () {
-                Navigator.of(context).push(
-                  MaterialPageRoute(
-                      builder: (context) => const IndepOrangeRev(),
-                  ),
-                );
-          },
-              child: Text("Українська музика з початку незалежності(1991) - помаранчева революція(2004)")
-          ),
+          ElevatedButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                    builder: (context) => const IndepOrangeRev(),
+                ),
+              );
+            },
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue, // background (button) color
+                foregroundColor: Colors.black, // foreground (text) color
+              ),
+            child: Text("Українська музика з початку незалежності(1991) - помаранчева революція(2004)")
+        ),
+          ElevatedButton(
+              onPressed: () {},
+              child: Text("2004 - 2014"),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.yellow, // background (button) color
+                foregroundColor: Colors.black, // foreground (text) color
+              ),
+        ),
+          ElevatedButton(
+              onPressed: () {},
+              child: Text("2014"),
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.yellow, // background (button) color
+                foregroundColor: Colors.black, // foreground (text) color
+            ),
+          )
         ],
       ),
+
+
+
+
+
+
+
+
+
+
+
+
+      // body: Column(
+      //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      //   children: [
+      //     ElevatedButton(
+      //         onPressed: () {
+      //           Navigator.of(context).push(
+      //             MaterialPageRoute(
+      //               builder: (context) => const Prerequisite(),
+      //             ),
+      //           );
+      //         },
+      //         child: Text("Що було до незалежності")
+      //     ),
+      //     ElevatedButton(
+      //         onPressed: () {
+      //           Navigator.of(context).push(
+      //             MaterialPageRoute(
+      //                 builder: (context) => const IndepOrangeRev(),
+      //             ),
+      //           );
+      //     },
+      //         child: Text("Українська музика з початку незалежності(1991) - помаранчева революція(2004)")
+      //     ),
+      //   ],
+      // ),
     );
   }
 }
